0a4ff29a32494da91fa64245c1044c53dde81176
nkosin <nkosi747@gmail.com>
2022-07-14 20:17:51 +0200

    Initial entry

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import SemiCircleProgress from "./assets/progress_bar/src";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SemiCircleProgress percentage={100} progressColor={"green"}>
+      <Text style={{ fontSize: 32, color: "green" }}>35%</Text>
+    </SemiCircleProgress>
+
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app</Text>
+    //   <StatusBar style="auto" />
+    // </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
