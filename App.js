cda1d62075d236332a5ede91f12f623132742233
nkosin <nkosi747@gmail.com>
2022-09-14 20:28:56 +0200

    finished month wheel picker

--- App.js
+++ App.js
@@ -1,27 +1,43 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView, StatusBar } from "react-native";
 import { SemiCircleProgressDisplay } from "./src/components/ui/progress_displays/SemiCircleProgress";
 import { ProgressBar, Button } from "react-native-paper";
 import { ItemsProgressBar } from "./src/components/ui/progress_displays/ItemsProgressBar";
 import { MainSummaryDisplay } from "./src/components/ui/MainSummaryDisplay";
+import { mockData } from "./src/components/ui/MockData";
+import { MonthYearPicker } from "./src/components/ui/menus/MonthYearPicker";
+import MonthProvider from "./src/store/MonthProvider";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      {/* <SemiCircleProgressDisplay spent={28000} total={30000} /> */}
-      {/* <ItemsProgressBar spent={25000} total={30000} /> */}
-      <MainSummaryDisplay spent={25000} total={30000} />
+  const maxDate = new Date(
+    Math.max(
+      ...mockData.map((element) => {
+        return new Date(element.date);
+      })
+    )
+  );
 
-      <StatusBar style="auto" />
-    </View>
+  return (
+    <>
+      <View style={styles.container}>
+        <MonthProvider>
+          {/* <MonthYearPicker /> */}
+          <MainSummaryDisplay />
+        </MonthProvider>
+      </View>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: StatusBar.currentHeight,
     flexDirection: "column",
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: "silver",
+    width: "100%",
   },
 });
